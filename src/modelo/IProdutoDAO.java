9566227f1f072d2609c5f85a461f8c0447d876b1
Kauã <kaua.bm@aluno.ifsc.edu.br>
2023-06-14 14:07:59 -0300

    Estud

---
+++ src/modelo/IProdutoDAO.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public interface IProdutoDAO {
+
+	public boolean inserir(Produto p);
+
+	public boolean alterar(Produto p);
+
+	public boolean excluir(Produto p);
+
+	public ArrayList<Produto> list();
+
+}
---
+++ src/modelo/Produto.java
@@ -0,0 +1,84 @@
+package modelo;
+
+public class Produto {
+	
+	public String nomePessoa;
+	public String getNomePessoa() {
+		return nomePessoa;
+	}
+	public void setNomePessoa(String nomePessoa) {
+		this.nomePessoa = nomePessoa;
+	}
+	public int getSenhaPessoa() {
+		return senhaPessoa;
+	}
+	public void setSenhaPessoa(int senhaPessoa) {
+		this.senhaPessoa = senhaPessoa;
+	}
+	public String getEmialPessoa() {
+		return emialPessoa;
+	}
+	public void setEmialPessoa(String emialPessoa) {
+		this.emialPessoa = emialPessoa;
+	}
+	public int getSenhaconfirmaPessoa() {
+		return senhaconfirmaPessoa;
+	}
+	public void setSenhaconfirmaPessoa(int senhaconfirmaPessoa) {
+		this.senhaconfirmaPessoa = senhaconfirmaPessoa;
+	}
+	public String getEmailPessoa() {
+		return emailPessoa;
+	}
+	public void setEmailPessoa(String emailPessoa) {
+		this.emailPessoa = emailPessoa;
+	}
+	public String getNomeCamisa() {
+		return nomeCamisa;
+	}
+	public void setNomeCamisa(String nomeCamisa) {
+		this.nomeCamisa = nomeCamisa;
+	}
+	public int getCodigoCatalago() {
+		return codigoCatalago;
+	}
+	public void setCodigoCatalago(int codigoCatalago) {
+		this.codigoCatalago = codigoCatalago;
+	}
+	public int getCodigoRoupa() {
+		return codigoRoupa;
+	}
+	public void setCodigoRoupa(int codigoRoupa) {
+		this.codigoRoupa = codigoRoupa;
+	}
+	public int senhaPessoa;
+	public String emialPessoa;
+	public int senhaconfirmaPessoa;
+	public int codigoCatalago;
+	public int codigoRoupa;
+	public String emailPessoa;
+	public String nomeCamisa;
+	public String nomeCalca;
+	public String nomeSapato;
+	public String nomeChapeu;
+	public String getNomeCalca() {
+		return nomeCalca;
+	}
+	public void setNomeCalca(String nomeCalca) {
+		this.nomeCalca = nomeCalca;
+	}
+	public String getNomeSapato() {
+		return nomeSapato;
+	}
+	public void setNomeSapato(String nomeSapato) {
+		this.nomeSapato = nomeSapato;
+	}
+	public String getNomeChapeu() {
+		return nomeChapeu;
+	}
+	public void setNomeChapeu(String nomeChapeu) {
+		this.nomeChapeu = nomeChapeu;
+	}
+	
+
+}
